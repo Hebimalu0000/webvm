7aab8943ed6490fac20b2d9ae8198e65307f9dce
Hebimalu0000 <hebimalu@outlook.com>
2025-02-15 23:01:12 +0900

    Update InformationTab.svelte

--- src/lib/InformationTab.svelte
+++ src/lib/InformationTab.svelte
@@ -1,11 +1,11 @@
 <h1 class="text-lg font-bold">Information</h1>
 <img src="assets/webvm_hero.png" alt="WebVM Logo" class="w-56 h-56 object-contain self-center">
-<p>WebVM is a virtual Linux environment running in the browser via WebAssembly</p>
-<p>It is based on:</p>
+<p>WebVM は、WebAssembly を介してブラウザ上で実行される仮想 Linux 環境です。</p>
+<p>ベースとなったもの:</p>
 <ul class="list-disc list-inside">
-	<li><a class="underline" target="_blank" href="https://cheerpx.io/">CheerpX</a>: x86 JIT in Wasm</li>
-	<li><a class="underline" target="_blank" href="https://xtermjs.org/">Xterm.js</a>: interactive terminal</li>
-	<li>Local/private <a class="underline" target="_blank" href="https://cheerpx.io/docs/guides/File-System-support">file storage</a></li>
-	<li><a class="underline" target="_blank" href="https://cheerpx.io/docs/guides/Networking">Networking</a> via <a class="underline" target="_blank" href="https://tailscale.com/">Tailscale</a></li>
+	<li><a class="underline" target="_blank" href="https://cheerpx.io/">CheerpX</a>: Wasm の x86 JIT</li>
+	<li><a class="underline" target="_blank" href="https://xtermjs.org/">Xterm.js</a>: 対話型ターミナル</li>
+	<li>ローカル/プライベート <a class="underline" target="_blank" href="https://cheerpx.io/docs/guides/File-System-support">ファイルストレージ</a></li>
+	<li><a class="underline" target="_blank" href="https://cheerpx.io/docs/guides/Networking">Networking</a> の経由システム: <a class="underline" target="_blank" href="https://tailscale.com/">Tailscale</a></li>
 </ul>
 <slot></slot>
