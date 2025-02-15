61d13dee35390820c7e9aea5999dd886cdb5a532
Hebimalu0000 <hebimalu@outlook.com>
2025-02-15 22:28:35 +0900

    Update CpuTab.svelte

--- src/lib/CpuTab.svelte
+++ src/lib/CpuTab.svelte
@@ -4,9 +4,8 @@
 </script>
 
 <h1 class="text-lg font-bold">Engine</h1>
-<PanelButton buttonImage="assets/cheerpx.svg" clickUrl="https://cheerpx.io/docs" buttonText="Explore CheerpX">
+<PanelButton buttonImage="assets/cheerpx.svg" clickUrl="https://cheerpx.io/docs" buttonText="CheerpX を探索する">
 </PanelButton>
-<p><span class="font-bold">Virtual CPU: </span>{$cpuPercentage}%</p>
-<p>CheerpX is a x86 virtualization engine in WebAssembly</p>
-<p>It can securely run unmodified x86 binaries and libraries in the browser</p>
-<p>Excited about our technology? <a class="underline" href="https://cheerpx.io/docs/getting-started" target="_blank">Start building</a> your projects using <a class="underline" href="https://cheerpx.io/" target="_blank">CheerpX</a> today!</p>
+<p><span class="font-bold">仮想 CPU 使用率: </span>{$cpuPercentage}%</p>
+<p>CheerpX は WebAssembly の x86 仮想化エンジンです</p>
+<p>未変更の x86 バイナリとライブラリをブラウザで安全に実行できます</p>
