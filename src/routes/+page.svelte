0a3382e33db01a5f594627ff9986b70796af96e2
Hebimalu0000 <hebimalu@outlook.com>
2025-02-16 20:10:37 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,5 +12,5 @@ function handleProcessCreated(processCount)
 </script>
 
 <WebVM configObj={configObj} processCallback={handleProcessCreated} cacheId="blocks_terminal">
-	<p>Looking for a complete desktop experience? Try the new <a class="underline" href="/alpine.html" target="_blank">Alpine Linux</a> graphical WebVM</p>
+	<p>WebVM を思う存分お楽しみください !</p>
 </WebVM>
